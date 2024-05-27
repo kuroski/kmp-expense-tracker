21209c169c831dbfc4f649b4faa3445e3a9aee45
kuroski <daniel.kuroski@jetbrains.com>
2024-05-27 09:07:45 +0200

    Env variables

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,4 +3,6 @@ plugins {
     // in each subproject's classloader
     alias(libs.plugins.jetbrainsCompose) apply false
     alias(libs.plugins.kotlinMultiplatform) apply false
+    alias(libs.plugins.buildKonfig) apply false
+    alias(libs.plugins.dotenvgradle)
 }
--- composeApp/src/desktopMain/kotlin/utils/Env.jvm.kt
+++
@@ -1,7 +0,0 @@
-package utils
-
-import io.github.cdimascio.dotenv.dotenv
-
-val dotenv = dotenv()
-
-actual fun getEnv(key: String): String? = System.getenv(key) ?: dotenv[key]
